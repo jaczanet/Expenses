13fe55a6da58c616dea0631c1bb2738c6c42e88a
jaczanet <hi@jacza.net>
2025-04-11 00:51:13 +0300

    Identifiable interface

---
+++ app/src/main/java/net/jacza/expenses/data/interfaces/Identifiable.java
@@ -0,0 +1,10 @@
+package net.jacza.expenses.data.interfaces;
+
+import java.util.UUID;
+
+/*
+ * Interface defining methods to identify an object by its UUID.
+ */
+public interface Identifiable {
+    public UUID getID();
+}
