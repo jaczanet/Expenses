da834260add10877269876209773cefd85500884
jaczanet <hi@jacza.net>
2025-04-10 20:36:00 +0300

    Update Transaction.java

--- app/src/main/java/net/jacza/expenses/data/model/Transaction.java
+++ app/src/main/java/net/jacza/expenses/data/model/Transaction.java
@@ -1,4 +1,20 @@
 package net.jacza.expenses.data.model;
 
+import java.time.LocalDate;
+
 public class Transaction {
+
+    private LocalDate date;
+    private Double amount;
+    private String note;
+    private Category category;
+    private Account account;
+
+    public Transaction(LocalDate date, Double amount, String note, Category category, Account account) {
+        this.date = date;
+        this.amount = amount;
+        this.note = note;
+        this.category = category;
+        this.account = account;
+    }
 }
