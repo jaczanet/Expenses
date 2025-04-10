653e7a995565c6fe4c73aa71ea0462bd477eaff1
jaczanet <hi@jacza.net>
2025-04-11 01:42:43 +0300

    Implemented logic for Account class

--- app/src/main/java/net/jacza/expenses/data/model/Account.java
+++ app/src/main/java/net/jacza/expenses/data/model/Account.java
@@ -1,4 +1,49 @@
 package net.jacza.expenses.data.model;
 
-public class Account {
+import java.util.ArrayList;
+import java.util.UUID;
+import net.jacza.expenses.data.interfaces.Identifiable;
+
+/*
+ * Data class wrapping account data.
+ */
+public class Account implements Identifiable {
+
+    private final UUID ID;
+
+    private String name;
+    private Double initialBalance;
+    private ArrayList<Transaction> transactions;
+
+    public Account(String name, Double initialBalance) {
+        this.ID = UUID.randomUUID();
+        this.name = name;
+        this.initialBalance = initialBalance;
+        this.transactions = new ArrayList<>();
+    }
+
+    @Override
+    public UUID getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
+
+    public Double getBalance() {
+        Double sum = initialBalance;
+        for (Transaction transaction : transactions) {
+            sum += transaction.getAmount();
+        }
+        return sum;
+    }
 }
