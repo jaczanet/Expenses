ac4f613667085ac3eda0a034e037d6bdd87f5869
jaczanet <hi@jacza.net>
2025-04-11 01:50:40 +0300

    UI layer package segregration

--- app/src/main/java/net/jacza/expenses/ui/MainActivity.java
+++ app/src/main/java/net/jacza/expenses/ui/MainActivity.java
@@ -1,12 +1,12 @@
-package net.jacza.expenses;
+package net.jacza.expenses.ui;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import net.jacza.expenses.R;
 
 public class MainActivity extends AppCompatActivity {
 
