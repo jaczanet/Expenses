a1d39f9ea3e704a4b37bd05a4f9e73fe356fbeb7
jaczanet <hi@jacza.net>
2025-04-11 14:58:36 +0300

    Improve code based on SOLID principles

--- app/src/main/java/net/jacza/expenses/data/interfaces/Repository.java
+++ app/src/main/java/net/jacza/expenses/data/interfaces/Repository.java
@@ -3,9 +3,9 @@ package net.jacza.expenses.data.interfaces;
 import java.util.ArrayList;
 
 /*
- * Interface defining methods of the CRUD design pattern for identifiable objects.
+ * Interface defining repository methods for the CRUD design pattern.
  */
-public interface Repository<T extends Identifiable> {
+public interface Repository<T> {
     public void create(T obj);
 
     public ArrayList<T> read();
