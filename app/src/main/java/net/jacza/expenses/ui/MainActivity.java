9a4df5216378a7cc005f6ca3ec626e41342f76dc
Gabriel C <gabriel.concepcion@pm.me>
2025-04-11 16:19:52 +0300

    Add initialization of BottomNavigationView and FrameLayout in MainActivity.java

--- app/src/main/java/net/jacza/expenses/ui/MainActivity.java
+++ app/src/main/java/net/jacza/expenses/ui/MainActivity.java
@@ -1,14 +1,22 @@
 package net.jacza.expenses.ui;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import net.jacza.expenses.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        frameLayout = findViewById(R.id.frame_layout);
+
+
+
+
     }
 }
