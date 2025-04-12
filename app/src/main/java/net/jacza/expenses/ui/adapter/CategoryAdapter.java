59e7a3ab1c12e0f5b7da7404e6b388d4312a8460
Gabriel C <gabriel.concepcion@pm.me>
2025-04-13 01:33:05 +0300

    Category management actions in UI

--- app/src/main/java/net/jacza/expenses/ui/adapter/CategoryAdapter.java
+++ app/src/main/java/net/jacza/expenses/ui/adapter/CategoryAdapter.java
@@ -1,6 +1,7 @@
 package net.jacza.expenses.ui.adapter;
 
 import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -10,19 +11,18 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.snackbar.Snackbar;
+
 import net.jacza.expenses.R;
 import net.jacza.expenses.data.base.Repository;
 import net.jacza.expenses.data.model.Category;
 import net.jacza.expenses.ui.activity.CategoryActivity;
-import net.jacza.expenses.ui.uistate.CategoryActivityUiState;
 
 import java.util.List;
 
 public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>{
     List<Category> categoryList;
     private Repository<Category> repository;
-
-    CategoryActivityUiState uiState;
 
     public CategoryAdapter(List<Category> categoryList, Repository<Category> repository) {
         this.categoryList = categoryList;
@@ -32,7 +32,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
     @NonNull
     @Override
     public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = View.inflate(parent.getContext(), R.layout.category_card, null);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.category_card, parent, false);
         return new CategoryViewHolder(view);
     }
 
@@ -48,13 +48,14 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
                 if (item.getItemId() == R.id.menu_edit) {
                     Intent intent = new Intent(view.getContext(), CategoryActivity.class);
                     intent.putExtra("MODE", "EDIT");
-                    intent.putExtra("EDITED CATEGORY", category);
+                    intent.putExtra("CATEGORY TO EDIT", category);
                     view.getContext().startActivity(intent);
                     return true;
                 } else if (item.getItemId() == R.id.menu_delete) {
                     // Delete the category
                     repository.delete(category);
-                    this.categoryList = repository.read();
+                    notifyCategoryRemoved(position, view);
+                    setList(repository.read());
                     return true;
                 }
                 return false;
@@ -68,6 +69,24 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
         return categoryList.size();
     }
 
+    public void setList(List<Category> newList) {
+        this.categoryList = newList;
+    }
+
+    public void notifyCategoryRemoved(int position, View view){
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, getItemCount());
+        Category tempCateg = categoryList.get(position);
+        String categName =tempCateg.getName();
+        Snackbar.make(view, "Undo Deletion of : " + categName, Snackbar.LENGTH_LONG)
+        .setAction ("UNDO", v -> {
+            categoryList.add(position, tempCateg);
+            repository.create(tempCateg);
+            notifyItemInserted(position);
+            notifyItemRangeChanged (position, getItemCount());
+        })
+        .show();
+    }
     public static class CategoryViewHolder extends RecyclerView.ViewHolder {
 
         private TextView categoryName;
