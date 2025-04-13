fab0ec1fdaf79a6cf4dfd39b0d1479fd357aace2
eenerere <enerel.tumurkhuu@gmail.com>
2025-04-14 00:16:37 +0300

    Populate account fragment with recyclerview and "add account" button

--- app/src/main/java/net/jacza/expenses/ui/fragment/AccountFragment.java
+++ app/src/main/java/net/jacza/expenses/ui/fragment/AccountFragment.java
@@ -1,66 +1,69 @@
 package net.jacza.expenses.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.flexbox.FlexboxLayoutManager;
+import com.google.android.flexbox.FlexDirection;
+import com.google.android.flexbox.FlexWrap;
+import com.google.android.flexbox.JustifyContent;
+
 import net.jacza.expenses.R;
+import net.jacza.expenses.data.base.Repository;
+import net.jacza.expenses.data.model.Account;
+import net.jacza.expenses.data.repository.AccountsRepository;
+import net.jacza.expenses.ui.activity.AccountActivity;
+import net.jacza.expenses.ui.adapter.AccountAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AccountFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AccountFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private AccountAdapter adapter;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+//    private Repository<Account> repo;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AccountFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AccountFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AccountFragment newInstance(String param1, String param2) {
-        AccountFragment fragment = new AccountFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public AccountFragment() { }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+        return view;
     }
+
+
+    @Override
+    public void onViewCreated( View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.addAccountBtn).setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), AccountActivity.class);
+            intent.putExtra("MODE", "ADD");
+            startActivity(intent);
+        });
+
+        recyclerView = view.findViewById(R.id.accountRecyclerView);
+
+        FlexboxLayoutManager flexboxLayoutManager = new FlexboxLayoutManager(requireContext());
+        flexboxLayoutManager.setFlexDirection(FlexDirection.ROW);
+        flexboxLayoutManager.setFlexWrap(FlexWrap.WRAP);
+        flexboxLayoutManager.setJustifyContent(JustifyContent.FLEX_START);
+
+//        repo = new AccountsRepository(getContext());
+//        adapter =new AccountAdapter(repo.read(),repo);
+        recyclerView.setLayoutManager(flexboxLayoutManager);
+        recyclerView.setAdapter(adapter);
+
+    }
+//    @Override
+//    public void onResume() {
+//        super.onResume();
+//        adapter.setList(repo.read());
+//        adapter.notifyDataSetChanged();
+//    } TODO uncomment after implementing repository
 }
