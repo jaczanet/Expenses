17cfedf978a5b34d26cbbf0cc8d1cbb51250d784
jaczanet <hi@jacza.net>
2025-04-13 21:07:54 +0300

    Update javadoc

--- app/src/main/java/net/jacza/expenses/data/base/DataSource.java
+++ app/src/main/java/net/jacza/expenses/data/base/DataSource.java
@@ -3,7 +3,7 @@ package net.jacza.expenses.data.base;
 import java.util.ArrayList;
 
 /*
- * Interface defining methods for data sources for storing and loading the data.
+ * Interface defining methods for data sources to store and load the data.
  */
 public interface DataSource<T> {
     ArrayList<T> load();
