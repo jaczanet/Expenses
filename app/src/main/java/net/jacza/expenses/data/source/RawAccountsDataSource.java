8493ac2ff9e63f7db89fd6f94843da0db80befc1
jaczanet <hi@jacza.net>
2025-04-13 23:48:57 +0300

    Init files

---
+++ app/src/main/java/net/jacza/expenses/data/source/RawAccountsDataSource.java
@@ -0,0 +1,6 @@
+package net.jacza.expenses.data.source;
+
+/*
+ * Data source of RawAccount, used by the repository layer to gather raw data.
+ */
+public class RawAccountsDataSource {}
---
+++ app/src/main/java/net/jacza/expenses/data/source/RawTransactionsDataSource.java
@@ -0,0 +1,6 @@
+package net.jacza.expenses.data.source;
+
+/*
+ * Data source of RawTransaction, used by the repository layer to gather raw data.
+ */
+public class RawTransactionsDataSource {}
