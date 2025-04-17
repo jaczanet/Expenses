d47da1ff125a29b247de906266f28b35898b76c5
jaczanet <hi@jacza.net>
2025-04-17 15:25:27 +0300

    Implement class for application wide context

---
+++ app/src/main/java/net/jacza/expenses/App.java
@@ -0,0 +1,20 @@
+package net.jacza.expenses;
+
+import android.app.Application;
+import android.content.Context;
+
+/*
+ * Class used to access application context.
+ */
+public class App extends Application {
+    private static Context context;
+
+    @Override public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+}
