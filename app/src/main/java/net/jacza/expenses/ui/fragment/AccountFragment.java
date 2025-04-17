12763941e712c9c235e99c90a023b9b001ba2045
eenerere <enerel.tumurkhuu@gmail.com>
2025-04-17 20:06:10 +0300

    Load Accounts into RecyclerView from repository.

--- app/src/main/java/net/jacza/expenses/ui/fragment/AccountFragment.java
+++ app/src/main/java/net/jacza/expenses/ui/fragment/AccountFragment.java
@@ -1,5 +1,6 @@
 package net.jacza.expenses.ui.fragment;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -23,10 +24,9 @@ import net.jacza.expenses.ui.adapter.AccountAdapter;
 import net.jacza.expenses.ui.util.SaveBtnModes;
 
 public class AccountFragment extends Fragment {
-    private RecyclerView recyclerView;
     private AccountAdapter adapter;
 
-//    private Repository<Account> repo;
+    private Repository<Account> repo;
 
     public AccountFragment() { }
 
@@ -48,23 +48,24 @@ public class AccountFragment extends Fragment {
             startActivity(intent);
         });
 
-        recyclerView = view.findViewById(R.id.accountRecyclerView);
+        RecyclerView recyclerView = view.findViewById(R.id.accountRecyclerView);
 
         FlexboxLayoutManager flexboxLayoutManager = new FlexboxLayoutManager(requireContext());
         flexboxLayoutManager.setFlexDirection(FlexDirection.ROW);
         flexboxLayoutManager.setFlexWrap(FlexWrap.WRAP);
         flexboxLayoutManager.setJustifyContent(JustifyContent.FLEX_START);
 
-//        repo = new AccountsRepository();
-//        adapter = new AccountAdapter(repo.read(),repo);
+        repo = AccountsRepository.getInstance();
+        adapter = new AccountAdapter(repo.read(),repo);
         recyclerView.setLayoutManager(flexboxLayoutManager);
         recyclerView.setAdapter(adapter);
 
     }
-//    @Override
-//    public void onResume() {
-//        super.onResume();
-//        adapter.setList(repo.read());
-//        adapter.notifyDataSetChanged();
-//    } TODO uncomment after implementing repository
+    @SuppressLint("NotifyDataSetChanged")
+    @Override
+    public void onResume() {
+        super.onResume();
+        adapter.setList(repo.read());
+        adapter.notifyDataSetChanged();
+    }
 }
