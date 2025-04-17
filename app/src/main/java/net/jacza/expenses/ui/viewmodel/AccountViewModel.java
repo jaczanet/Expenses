05ec165f119cee0ddd69a11de01ca112e0e547b3
Gabriel C <gabriel.concepcion@pm.me>
2025-04-17 16:01:35 +0300

    AccountViewModel cleanup

--- app/src/main/java/net/jacza/expenses/ui/viewmodel/AccountViewModel.java
+++ app/src/main/java/net/jacza/expenses/ui/viewmodel/AccountViewModel.java
@@ -3,7 +3,7 @@ package net.jacza.expenses.ui.viewmodel;
 import android.app.Application;
 
 import androidx.annotation.NonNull;
-import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.ViewModel;
 
 import net.jacza.expenses.data.base.Repository;
 import net.jacza.expenses.data.model.Account;
@@ -11,12 +11,9 @@ import net.jacza.expenses.data.repository.AccountsRepository;
 
 import java.util.ArrayList;
 
-public class AccountViewModel extends AndroidViewModel {
-    // private final Repository<Account> repo; TODO uncomment after implementing AccountsRepository
-    public AccountViewModel(@NonNull Application application) {
-        super(application);
-        // repo = new AccountsRepository(application.getApplicationContext());
-    }
+public class AccountViewModel extends ViewModel {
+    // TODO: uncomment after implementing AccountsRepository
+//    private final Repository<Account> repo = new AccountsRepository();
 //    public void create(Account account) {repo.create(account);}
 //    public void delete(Account account) {repo.delete(account);}
 //    public ArrayList<Account> read() {return repo.read();}
