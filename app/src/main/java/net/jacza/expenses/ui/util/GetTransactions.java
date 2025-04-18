ac80296d444e0ebf303f1ed62606e28f065ba60f
jaczanet <hi@jacza.net>
2025-04-18 23:24:42 +0300

    utility to get filtered transactions

---
+++ app/src/main/java/net/jacza/expenses/ui/util/GetTransactions.java
@@ -0,0 +1,32 @@
+package net.jacza.expenses.ui.util;
+
+import java.util.ArrayList;
+import net.jacza.expenses.data.base.Repository;
+import net.jacza.expenses.data.model.Account;
+import net.jacza.expenses.data.model.Category;
+import net.jacza.expenses.data.model.Transaction;
+import net.jacza.expenses.data.repository.TransactionsRepository;
+
+/*
+ * Methods to get the list of transactions filtered by category or account.
+ */
+public class GetTransactions {
+
+    private static final Repository<Transaction> transRepo = TransactionsRepository.getInstance();
+
+    // override the constructor to avoid instantiation
+    private GetTransactions() {}
+
+    // static methods
+    public static ArrayList<Transaction> byCategory(Category entry) {
+        var transactions = transRepo.read();
+        transactions.removeIf(item -> !item.getCategory().getID().equals(entry.getID()));
+        return transactions;
+    }
+
+    public static ArrayList<Transaction> byAccount(Account entry) {
+        var transactions = transRepo.read();
+        transactions.removeIf(item -> !item.getAccount().getID().equals(entry.getID()));
+        return transactions;
+    }
+}
