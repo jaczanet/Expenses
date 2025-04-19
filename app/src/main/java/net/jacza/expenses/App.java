dd8daa72dbd045b89199a89b97a8662d04ddce17
jaczanet <hi@jacza.net>
2025-04-19 17:46:46 +0300

    Refactor code

--- app/src/main/java/net/jacza/expenses/App.java
+++ app/src/main/java/net/jacza/expenses/App.java
@@ -7,9 +7,11 @@ import android.content.Context;
  * Class used to access application context.
  */
 public class App extends Application {
+
     private static Context context;
 
-    @Override public void onCreate() {
+    @Override
+    public void onCreate() {
         super.onCreate();
         context = getApplicationContext();
     }
