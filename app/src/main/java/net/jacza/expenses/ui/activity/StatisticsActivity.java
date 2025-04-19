5a430881d8475bd0d6122db3bd84db4f35a17659
Gabriel C <gabriel.concepcion@pm.me>
2025-04-19 19:59:32 +0300

    Set up statistics screen and added basic monthly statistics

--- app/src/main/java/net/jacza/expenses/ui/activity/StatisticsActivity.java
+++ app/src/main/java/net/jacza/expenses/ui/activity/StatisticsActivity.java
@@ -8,15 +8,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import net.jacza.expenses.R;
+import net.jacza.expenses.domain.analytics.GetStatisticsUseCase;
+import net.jacza.expenses.ui.adapter.StatisticsAdapter;
 
 public class StatisticsActivity extends AppCompatActivity {
     private Button  backBtn;
+    private RecyclerView recyclerView;
+    private StatisticsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setUpEdgeToEdge();
+
+        backBtn = findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(v -> finish());
+
+        recyclerView = findViewById(R.id.statsRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new StatisticsAdapter(GetStatisticsUseCase.getMonthlyStatistics());
+        recyclerView.setAdapter(adapter);
+
+
+
+    }
+
+    private void setUpEdgeToEdge(){
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_statistics);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -24,8 +45,6 @@ public class StatisticsActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        backBtn = findViewById(R.id.backBtn);
-        backBtn.setOnClickListener(v -> finish());
     }
 
 }
