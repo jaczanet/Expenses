cfd806d9ab1fd6cc3304e981181f57ffd7a90b92
Gabriel C <gabriel.concepcion@pm.me>
2025-04-20 12:20:12 +0300

    Interface OnCategoryClickListener documentation

--- app/src/main/java/net/jacza/expenses/ui/interfaces/OnCategoryClickListener.java
+++ app/src/main/java/net/jacza/expenses/ui/interfaces/OnCategoryClickListener.java
@@ -2,6 +2,9 @@ package net.jacza.expenses.ui.interfaces;
 
 import net.jacza.expenses.data.model.Category;
 
+/**
+ * Interface definition for a callback to be invoked when a category is clicked.
+ */
 public interface OnCategoryClickListener {
     void onCategoryClick(Category category);
 }
